47aec1ff29138c36bbec6f4bcac76c71796f41e8
npyl <ulcheats@gmail.com>
2021-07-28 13:31:47 +0300

    we now support words

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-flex lexer.l
 bison -v -d syntax.y
+flex lexer.l
 gcc -c syntax.tab.c lex.yy.c
 gcc syntax.tab.o lex.yy.o -o parser
--- tests/test1.c
+++ tests/test1.c
@@ -1,1 +1,17 @@
-PROGRAM a;
+PROGRAM test1;
+FUNCTION myfunc1(INT i, CHAR c)
+    STRUCT
+    VARS
+        INT     i2;
+        CHAR    var1, var2, var3[10], var4;
+    ENDSTRUCT
+
+    RETURN 0;
+END_FUNCTION
+STARTMAIN
+    STRUCT
+    VARS
+        INT     i2;
+        CHAR    var1, var2, var3[10], var4;
+    ENDSTRUCT
+ENDMAIN
